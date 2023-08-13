617a3cabd4556b9dedf30eb0e2d5094809080845
ijaz35 <ijaz.hossain1@gmail.com>
2023-08-13 23:02:54 +0600

    action payload & action creator added

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const counterEl = document.getElementById('counterEl');
 const increaseBtn = document.getElementById('increment');
 const decreaseBtn = document.getElementById('decrement');
 
+// action identifier
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+
+// action creators
+const increment = (value) => {
+    return {
+        type: INCREMENT,
+        payload: value,
+    }
+}
+const decrement = (value) => {
+    return {
+        type: DECREMENT,
+        payload: value,
+    }
+}
 // initial state
 const initialState = {
     value: 0,
@@ -12,34 +29,35 @@ function counterReducer(state = initialState, action) {
     if (action.type === 'increment') {
         return {
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         };
     } else if (action.type === 'decrement') {
         return {
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         }
     } else {
         return state;
     }
 }
+
 // create store
-const store = Redux.createStore(counterReducer)
+const store = Redux.createStore(counterReducer);
+
+// render function
 const render = () => {
     const state = store.getState();
     counterEl.innerText = state.value.toString();
 }
 render();
-store.subscribe(render)
-// add event listener or action
 
+// subscribe to store
+store.subscribe(render)
+
+// add event listener or action
 increaseBtn.addEventListener('click', () => {
-    store.dispatch({
-        type: 'increment'
-    });
+    store.dispatch(increment(5));
 });
 decreaseBtn.addEventListener('click', () => {
-    store.dispatch({
-        type: 'decrement'
-    });
+    store.dispatch(decrement(2));
 });
