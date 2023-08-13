8b860c84cbd4670876563bb71281f3312bda715d
ijaz35 <ijaz.hossain1@gmail.com>
2023-08-13 23:05:01 +0600

    action type updated

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ const initialState = {
 
 // create reducer function
 function counterReducer(state = initialState, action) {
-    if (action.type === 'increment') {
+    if (action.type === INCREMENT) {
         return {
             ...state,
             value: state.value + action.payload,
         };
-    } else if (action.type === 'decrement') {
+    } else if (action.type === DECREMENT) {
         return {
             ...state,
             value: state.value - action.payload,
